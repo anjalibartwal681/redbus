51903617628d63cf5c57be62d618f6316ce4a873
anjalibartwal681 <anjalibartwal681@gmail.com>
2026-04-02 07:13:14 +0530

    first commit

--- src/data/buses.js
+++ src/data/buses.js
@@ -40,7 +40,7 @@ export const buses = [
     image: "https://tse4.mm.bing.net/th/id/OIP.LL70emYEeSDy4ib1Olx0dAHaEK?pid=Api&P=0&h=180"
   },
 ];
-// llll
+// llll for commmit
 
 export const cities = [
   "Delhi",
